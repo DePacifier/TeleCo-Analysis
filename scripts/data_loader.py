72d3e3e1855637260347524746c100fc59580ec2
DePacifier <37738957+DePacifier@users.noreply.github.com>
2021-07-12 18:50:58 +0300

    started working on data cleaning

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -13,7 +13,7 @@ def load_df_from_csv(filename: str) -> pd.DataFrame:
 def load_df_from_excel(filename: str) -> pd.DataFrame:
     """A simple function which tries to load a dataframe from a specified .xslx filename"""
     try:
-        df = pd.read_excel(filename, engine='openpyxl')
+        df = pd.read_excel(filename, na_values=['?', None], engine='openpyxl')
         return df
     except:
         print("Error Occured:\n\tCould not find specified .xslx file")

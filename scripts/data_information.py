c17979b6b837698c4b206896be971811d3b9e943
DePacifier <37738957+DePacifier@users.noreply.github.com>
2021-07-12 18:07:46 +0300

    done initial data information exploration

--- scripts/data_information.py
+++ scripts/data_information.py
@@ -5,19 +5,38 @@ class DataInfo:
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
+    def get_basic_description(self):
+        self.get_size()
+        self.get_total_memory_usage()
+        self.get_memory_usage()
+        self.get_information()
+
+    def get_missing_description(self):
+        self.get_total_missing_values()
+        self.get_columns_with_missing_values()
+        self.get_column_based_missing_values()
+
+    def get_columns(self):
+        print("Columns Listed in the DataFrame are: ")
+        return self.df.columns.tolist()
+
     def get_information(self):
+        print("DataFrame Information: ")
         return self.df.info()
 
     def get_size(self):
+        value = self.df.shape
         print(
-            f"The DataFrame containes {self.db.shape[0]} rows and {self.db.shape[1]} columns.")
-        return self.db.shape
+            f"The DataFrame containes {value[0]} rows and {value[1]} columns.")
+        return value
 
     def get_total_entries(self):
-        print(f"The DataFrame containes {self.db.size} entries.")
-        return self.db.size
+        value = self.df.size
+        print(f"The DataFrame containes {value} entries.")
+        return value
 
     def get_description(self):
+        print("DataFrame Description: ")
         return self.df.describe()
 
     def get_column_description(self, column_name: str):
@@ -45,23 +64,47 @@ class DataInfo:
             print("Failed to get decription of the column")
 
     def get_total_missing_values(self):
-        value = self.df.isnull().sum().sum()
-        print(f"The total number of missing values is {value}")
-        return value
+        missing_entries = self.df.isnull().sum().sum()
+        total_entries = self.df.size
+        print(f"The total number of missing values is {missing_entries}")
+        print(round(((missing_entries/total_entries) * 100), 2),
+              "%", "missing values.")
+        return missing_entries
 
     def get_columns_with_missing_values(self):
         lst = self.df.isnull().any()
-        print(f"Columns with missing values are:\n{lst}")
-        return lst
+        # print(f"Columns with missing values are:\n{lst}")
+        return lst.index
 
     def get_column_based_missing_values(self):
         value = self.df.isnull().sum()
-        print(value)
+        return value
+
+    def get_column_based_missing_percentage(self):
+        col_null = self.get_column_based_missing_values()
+        total_entries = self.df.shape[0]
+        missing_percentage = []
+        for col_missing_entries in col_null:
+            value = str(
+                round(((col_missing_entries/total_entries) * 100), 2)) + " %"
+            missing_percentage.append(value)
+
+        missing_df = pd.DataFrame(col_null, columns=['total_missing_values'])
+        missing_df['missing_percentage'] = missing_percentage
+        return missing_df
+
+    def get_columns_missing_percentage_greater_than(self, num: float):
+        all_cols = self.get_column_based_missing_percentage()
+        extracted = all_cols['missing_percentage'].str.extract(r'(.+)%')
+        return extracted[extracted[0].apply(lambda x: float(x) >= num)].index
+
+    def get_total_memory_usage(self):
+        value = self.df.memory_usage(deep=True).sum()
+        print(f"Current DataFrame Memory Usage:\n{value}")
         return value
 
     def get_memory_usage(self):
-        print(
-            f"The Dataframe is currently using {self.df.memory_usage()} bytes.")
+        print(f"Current DataFrame Memory Usage of columns is :")
         return self.df.memory_usage()
 
     def get_aggregate(self, stat_list: list):
